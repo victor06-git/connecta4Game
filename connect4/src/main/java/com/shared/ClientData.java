cff4e0edb8b340d8e10d613b095425d343cfa4ff
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-10-06 18:37:22 +0200

    Fixing wrong new json creation

--- connect4/src/main/java/com/shared/ClientData.java
+++ connect4/src/main/java/com/shared/ClientData.java
@@ -54,7 +54,8 @@ public class ClientData {
         String name = obj.optString("name", null);
         String color = obj.optString("color", null);
 
-        ClientData cd = new ClientData(name, color);
+        ClientData cd = new ClientData(name);
+        cd.color = obj.optString("color", "BLACK");
         cd.mouseX = obj.optInt("mouseX", -1);
         cd.mouseY = obj.optInt("mouseY", -1);
         cd.row = obj.optInt("row", -1);
