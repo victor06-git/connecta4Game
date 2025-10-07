2521d7d113987b3ce0efbf7d4dd126cc7053ad24
victor06-git <asensiovector@gmail.com>
2025-10-07 22:34:27 +0200

    views and subviews controllers updated

--- connect4/src/main/java/com/connect4/CtrlOpponentSelection.java
+++ connect4/src/main/java/com/connect4/CtrlOpponentSelection.java
@@ -1,8 +1,13 @@
 package com.connect4;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class CtrlOpponentSelection {
-    
+
     @FXML
     private VBox list_send, list_receive;
+
+    // Cargar las subviews en cada VBox
 
 }
